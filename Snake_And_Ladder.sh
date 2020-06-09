2f793465362e991c45e489fb4ab2af09b03eb71c
Srijan-18 <srijan.mishrasir@gmail.com>
2020-06-09 16:53:02 +0530

    Die roll count and player position at each roll reported

--- Snake_And_Ladder.sh
+++ Snake_And_Ladder.sh
@@ -1,11 +1,11 @@
-#!/bin/bash -x
 
 echo "********		Welcome to Snake and Ladder Game		********"
 START_POSITION=0
 WINNING_POSITION=100
 LADDER=1
 SNAKE=2
-
+rollCount=0
+declare -a playerPositions
 function eventCheck()
 {
 
@@ -30,11 +30,11 @@ function eventCheck()
 function game()
 {
 	playerCurrentPosition=$START_POSITION
-
 	while ((playerCurrentPosition<WINNING_POSITION))
 	do
 
-		dieResult=$(((RANDOM%6)+1))
+		dieResult=$(((RANDOM%6)+1));
+		((rollCount++))
 		eventResult=$((RANDOM%3))
 		playerCurrentPosition=$(eventCheck $eventResult)
 		if (( playerCurrentPosition<START_POSITION ))
@@ -44,7 +44,12 @@ function game()
 		then
 			playerCurrentPosition=$(( playerCurrentPosition - dieResult ))
 		fi
+		playerPositions[$rollCount]=$playerCurrentPosition
 	done
-	echo $playerCurrentPosition
-}
-player1CurrentPosition=$(game)
+	}
+game;
+echo "Number of Rolls: $rollCount "
+for (( counter=1;counter<=rollCount;counter++ ))
+do
+	echo "Roll$counter:  ${playerPositions[$counter]}"
+done
