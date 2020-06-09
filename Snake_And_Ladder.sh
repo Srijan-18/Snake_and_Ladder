1de631845c04329aa194ce91b112606124bf4bf2
Srijan-18 <srijan.mishrasir@gmail.com>
2020-06-09 15:13:17 +0530

    Conditon for player position always greater than or equal to zero realised

--- Snake_And_Ladder.sh
+++ Snake_And_Ladder.sh
@@ -2,20 +2,21 @@
 
 echo "********		Welcome to Snake and Ladder Game		********"
 START_POSITION=0
+WINNING_POSITION=100
 LADDER=1
 SNAKE=2
 
 function eventCheck()
 {
 
-	case in $1
+	case $1 in
 
 		$LADDER)
-			player1CurrentPosition=$((player1CurrentPosition + diceResult))
+			player1CurrentPosition=$((player1CurrentPosition + dieResult))
 		;;
 
 		$SNAKE)
-			player1CurrentPosition=$((player1CurrentPosition - diceResult))
+			player1CurrentPosition=$((player1CurrentPosition - dieResult))
 		;;
 
 		*)
@@ -26,7 +27,14 @@ function eventCheck()
 }
 
 player1CurrentPosition=$START_POSITION
-dieResult=$(((RANDOM%6)+1))
-eventResult=$((RANDOM%3))
-player1CurrentPosition=$(eventCheck $eventResult)
+while ((player1CurrentPosition<WINNING_POSITION))
+do
 
+	dieResult=$(((RANDOM%6)+1))
+	eventResult=$((RANDOM%3))
+	player1CurrentPosition=$(eventCheck $eventResult)
+	if (( player1CurrentPosition<0 ))
+	then
+		player1CurrentPosition=$START_POSITION
+	fi
+done
