a8f41511420a3964de8e17f6f37eba90810a15dc
Srijan-18 <srijan.mishrasir@gmail.com>
2020-06-09 15:02:08 +0530

    Implementation of event conditions of Ladder,Snake & No play

--- Snake_And_Ladder.sh
+++ Snake_And_Ladder.sh
@@ -2,5 +2,31 @@
 
 echo "********		Welcome to Snake and Ladder Game		********"
 START_POSITION=0
+LADDER=1
+SNAKE=2
+
+function eventCheck()
+{
+
+	case in $1
+
+		$LADDER)
+			player1CurrentPosition=$((player1CurrentPosition + diceResult))
+		;;
+
+		$SNAKE)
+			player1CurrentPosition=$((player1CurrentPosition - diceResult))
+		;;
+
+		*)
+		#default is no play condition
+		;;
+	esac
+	echo $player1CurrentPosition
+}
+
 player1CurrentPosition=$START_POSITION
-dieResult=(((RANDOM%6)+1))
+dieResult=$(((RANDOM%6)+1))
+eventResult=$((RANDOM%3))
+player1CurrentPosition=$(eventCheck $eventResult)
+
